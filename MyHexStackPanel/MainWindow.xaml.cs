ccc6c11e178c16048deed4321ed866a5d49b7247
Uwe on APOS-NBW10-02 <uhw@gmx.org>
2017-03-11 22:54:17 +0100

    Start.

---
+++ MyHexStackPanel/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyHexStackPanel
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,  INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        static Random _random = new Random();
+
+        #region INotify Propertie Changed
+        private HexMessageContainerUCAction containerUCAction;
+        public HexMessageContainerUCAction ContainerUCAction
+        {
+            get { return containerUCAction; }
+            set
+            {
+                if (value != ContainerUCAction)
+                {
+                    containerUCAction = value;
+                    OnPropertyChanged("ContainerUCAction");
+                };
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            ContainerUCAction = HexMessageContainerUCAction.Instance;
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        #endregion
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        /// <summary>
+        /// Grid_MouseDown
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left || e.ChangedButton == MouseButton.Middle)
+                AddARandomTestLine(Direction.In);
+            else
+                AddARandomTestLine(Direction.Out);
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// AddARandomLine
+        /// </summary>
+        private void AddARandomTestLine(Direction direction)
+        {
+            byte[] barray = new byte[(int)RandomDouble(1, 100, 0)];
+
+            _random.NextBytes(barray);
+
+            // Just for testing with a fix and defined sream
+            //barray = new byte[]{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x66 };
+
+            ContainerUCAction.AddMessage(barray,direction);
+        }
+
+        /// <summary>
+        /// RandomDouble
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="deci"></param>
+        /// <returns></returns>
+        public static double RandomDouble(double min, double max, int deci)
+        {
+            double d;
+            d = _random.NextDouble() * (max - min) + min;
+            return Math.Round(d, deci);
+        }
+
+        /// <summary>
+        /// OnPropertyChanged
+        /// </summary>
+        /// <param name="p"></param>
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+
+        #endregion
+    }
+}
